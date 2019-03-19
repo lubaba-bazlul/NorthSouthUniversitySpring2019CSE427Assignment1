2172b85674994aefae9543b9273f5f761ffe82a7
lubaba-bazlul <lubaba.bazlul@northsouth.edu>
2019-03-20 02:42:59 +0600

    GenericStack Implimentation

--- North South University/Spring 2019/CSE427/Assignment 1/Assignment 1/Assignment 1/src/tStackTest/StackElement.java
+++ North South University/Spring 2019/CSE427/Assignment 1/Assignment 1/Assignment 1/src/tStackTest/StackElement.java
@@ -1,5 +1,33 @@
 package tStackTest;
 
-public class StackElement {
+public class StackElement<T> {
+T element;
+StackElement<T> next;
+
+public StackElement(T element, StackElement next){
+	this.element = element;
+	this.next= next;
+}
+
+
+public StackElement<T> getNext(){
+	return next;
+}
+
+
+public T getElement(){
+	return element;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 }
